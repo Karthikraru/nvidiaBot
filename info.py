ddb0335ff04562a985473d139b83d54a07084f61
Karthik <karthik@Karthiks-iMac.local>
2020-09-29 19:10:08 -0500

    Initial commit

---
+++ info.py
@@ -0,0 +1,13 @@
+info = {
+    'first': 'First',
+    'last': 'Last',
+    'address': '123 House Rd',
+    'city': 'City',
+    'phone': '1112223333',
+    'email': 'maiL@gmail.com',
+    'zip': '12345-1234',
+    'card': '1111222233334444',
+    'expmonth': '2', #month, january = 2 december = 13
+    'expyear': '2', #years after 2018
+    'cvv': '123',
+}
---
+++ nvidia.py
@@ -0,0 +1,133 @@
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, \
+    StaleElementReferenceException, ElementClickInterceptedException
+from selenium import webdriver
+import time
+from info import info
+
+def timeme(method):
+    def wrapper(*args, **kw):
+        startTime = int(round(time.time() * 1000))
+        result = method(*args, **kw)
+        endTime = int(round(time.time() * 1000))
+        print('Checked out in ', (endTime - startTime) / 1000, 's')
+        return result
+
+    return wrapper
+
+@timeme
+def order():
+    #Go to cart
+    while True:
+        try:
+            driver.find_element_by_xpath('//*[@id="cart"]/div[2]').click()
+            break
+        except NoSuchElementException:
+            pass
+        except StaleElementReferenceException:
+            pass
+
+    #Checkout as guest
+    while True:
+        try:
+            driver.find_element_by_xpath('//*[@id="btnCheckoutAsGuest"]').click()
+            break
+        except NoSuchElementException:
+            pass
+        except StaleElementReferenceException:
+            pass
+
+    #First Name
+    while True:
+        try:
+            driver.find_element_by_xpath('//*[@id="billingName1"]').send_keys(info['first'])
+            break
+        except NoSuchElementException:
+            pass
+        except StaleElementReferenceException:
+            pass
+
+    #Last name
+    driver.find_element_by_xpath('//*[@id="billingName2"]').send_keys(info['last'])
+
+    #Address
+    driver.find_element_by_xpath('//*[@id="billingAddress1"]').send_keys(info['address'])
+
+    #City
+    driver.find_element_by_xpath('//*[@id="billingCity"]').send_keys(info['city'])
+
+    #Zip
+    driver.find_element_by_xpath('//*[@id="billingPostalCode"]').send_keys(info['zip'])
+
+    #Phone
+    driver.find_element_by_xpath('//*[@id="billingPhoneNumber"]').send_keys(info['phone'])
+
+    #Email
+    driver.find_element_by_xpath('//*[@id="email"]').send_keys(info['email'])
+    driver.find_element_by_xpath('//*[@id="verEmail"]').send_keys(info['email'])
+
+    #Card
+    driver.find_element_by_xpath('//*[@id="ccNum"]').send_keys(info['card'])
+
+    #ExpMonth
+    driver.find_element_by_xpath('//*[@id="expirationDateMonth"]/option[{}]'.format(info['expmonth'])).click()
+
+    #ExpYear
+    driver.find_element_by_xpath('//*[@id="expirationDateYear"]/option[{}]'.format(info['expyear'])).click()
+
+    #CVV
+    driver.find_element_by_xpath('//*[@id="cardSecurityCode"]').send_keys(info['cvv'])
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome('./chromedriver')
+    link = input("What is the product url?\n")
+    refreshDelay = 0
+    while True:
+        refreshDelay = input("\nWhat refresh delay would you like? NOTE: Enter in terms of seconds. You can use decimals.\n")
+        try:
+            float(refreshDelay)
+            break
+        except ValueError:
+            print('\nInvalid value. Enter as a number. Ex: "2" for two seconds.')
+            pass
+
+    driver.get(link)
+    notify = False
+    while True:
+        try:
+            driver.find_element_by_xpath('//*[@id="3070-staticprice"]')
+            time.sleep(refreshDelay)
+            driver.refresh()
+        except NoSuchElementException:
+            notify = not notify
+            pass
+        except StaleElementReferenceException:
+            notify = not notify
+            pass
+        except ElementNotInteractableException:
+            notify = not notify
+            pass
+
+        if notify:
+            try:
+                driver.find_element_by_xpath('//*[@id="overview_sld1"]/div[1]/div[1]/div/div/div/div[5]/div/div/div/div[2]/a').click()
+                break
+            except NoSuchElementException:
+                pass
+            except StaleElementReferenceException:
+                pass
+            except ElementNotInteractableException:
+                pass
+            except ElementClickInterceptedException:
+                pass
+
+            try:
+                driver.find_element_by_xpath('//*[@id="cart"]/div[2]')
+                break
+            except NoSuchElementException:
+                pass
+            except StaleElementReferenceException:
+                pass
+            except ElementNotInteractableException:
+                pass
+    order()
